c98ba243e4454750014fdfd05d88ff709bcc50e3
Abdelrahman Ahmed <73317105+abdelrahman252@users.noreply.github.com>
2022-09-06 05:52:01 +0100

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-
-
-import './App.css'
-
+import "./App.scss";
+import Header from "./components/navbar/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Banner from "./components/banner/Banner";
 function App() {
-
-
   return (
-    <div className="App">
-      here we build
+    <div className="app">
+      <Header />
+      <Banner />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
